754860d11ceee7d3106424a4e3d64f8c6e305056
Jashaswimalya Acharjee <31704400+jash-maester@users.noreply.github.com>
2019-12-26 20:50:00 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-function initMap() {
-    // Your location
-    const loc = { lat: 42.361145, lng: -71.057083 };
-
-    const map = new google.maps.Map(document.querySelector('.map'), {
-        zoom: 14,
-        center: loc
-    });
-    //The marker, positioned at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
-}
+// Map
+var mymap = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'sk.eyJ1IjoiamFzaG1hZXN0ZXIiLCJhIjoiY2s0bXV5b3QzMG92ZjNtbWlncGNvaDJmZyJ9.uTsP83Vu5ap6JST-YsjNxQ'
+}).addTo(mymap);
+var marker = L.marker([23.841205, 91.285187]).addTo(mymap);
+marker.bindPopup("<b>Hello world!</b><br>I am a showing Jash's Location.").openPopup();
 
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
@@ -33,3 +35,6 @@ window.addEventListener('scroll', function() {
         document.querySelector('#navbar').style.opacity = 1;
     }
 });
+
+// Map Token
+// sk.eyJ1IjoiamFzaG1hZXN0ZXIiLCJhIjoiY2s0bXV5b3QzMG92ZjNtbWlncGNvaDJmZyJ9.uTsP83Vu5ap6JST-YsjNxQ
